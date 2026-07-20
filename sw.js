e85556da199583dd64c926d26f28dadc28f64cae
chidiezeibieli <chidiezeibieli@gmail.com>
2026-07-20 23:04:05 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tbt-scanner-v2";
+const CACHE_NAME = "tbt-scanner-v3";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
